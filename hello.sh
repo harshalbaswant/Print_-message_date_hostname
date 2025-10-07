5c8d9459865cbcc7f2cae00682f9cdb3cb15d3f0
Harshal Baswant <49876293+harshalbaswant@users.noreply.github.com>
2025-10-07 15:28:11 +0530

    Add files via upload

---
+++ hello.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "******************************************************************"
+echo "It is a simple script to print the date and hostname of your system"
+echo "******************************************************************"
+
+sleep 2
+
+echo "***Current Date and time:";date
+
+sleep 2
+
+echo "***Hostname:";hostname
+
+
